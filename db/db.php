df3d93de8aa27a6dba59441c22267863b04e8a9d
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-07-07 13:26:46 +0200

    modifiche db

--- db/db.php
+++ db/db.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../classes/Category.php';
 require_once __DIR__ . '/../classes/Food.php';
 require_once __DIR__ . '/../classes/Toy.php';
 require_once __DIR__ . '/../classes/Accessory.php';
+require_once __DIR__ . '/../traits/HasDimension.php';
 
 
 // $categories = [
